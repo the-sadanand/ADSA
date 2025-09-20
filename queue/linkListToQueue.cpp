103dde01f34e58f205c752760ba82f970378e06f
the-sadanand <thesadanand.toponed0@gmail.com>
2025-09-20 23:34:25 +0530

    Queue with help of Link List

---
+++ queue/linkListToQueue.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val=val;
+        this->next = NULL;
+    }
+};
+
+class Queue{
+public:
+    Node* tail;
+    Node* head;
+    int s;
+    Queue(){
+        head=tail=NULL;
+        s=0;
+    }
+    void push(int val){
+        Node* temp = new Node(val);
+        if(s==0) head=tail= temp;
+        else{
+            tail->next = temp;
+            tail=temp;
+        }
+        s++;
+    }
+
+    void pop(){
+        if(s==0){
+            cout<<"Queue is empty!"<<endl;
+            return;
+        }
+        head= head->next;
+        s--;
+    }
+    int front(){
+        if(s==0){
+            cout<<"Queue is empty!"<<endl;
+            return -1;
+        } 
+        return head->val;       
+    }
+
+    int back(){
+        if(s==0){
+            cout<<"Queue is empty!"<<endl;
+            return -1;
+        }
+        return tail->val;
+    }
+    void display(){
+        if(s==0){
+            cout<<"Queue is empty!"<<endl;
+        }
+        Node* temp = head;
+        while(temp){
+            cout<<temp->val<<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+    int size(){
+        return s;
+    }
+
+    bool empty(){
+        if (s==0) return true;
+        else return false;
+    }
+
+};
+
+int main(){
+    Queue q;
+    q.push(10);
+    q.push(20);
+    q.push(40);
+    q.push(60);
+    q.push(70);
+    q.push(60);
+    q.push(00);
+    q.push(20);
+    q.display();
+    cout<<q.size()<<endl;
+    q.pop();
+    q.display();
+    cout<<q.size();
+}
