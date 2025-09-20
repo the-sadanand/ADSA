163a738614af530d1ddf5381e082549f6d7bb038
the-sadanand <thesadanand.toponed0@gmail.com>
2025-09-20 18:27:30 +0530

    learn to display queue , and preserve it

--- queue/basic.cpp
+++ queue/basic.cpp
@@ -16,4 +16,27 @@ int main(){
     q.pop();
     cout<<q.size()<<endl;
     cout<<q.front()<<endl;
+    // q.back(45);
+    cout<<q.back()<<endl; // its print what is in rear side of queue 
+
+    // another queue to learn how to display it and save as it
+    // t.c = 1 and s.c = 1 
+    cout<<" display the queue and keep it same "<<endl;
+    queue<int>display;
+    display.push(12);
+    display.push(13);
+    display.push(14);
+    display.push(15);
+    display.push(16);
+    int size= display.size();
+    for(int i =0 ;i<size ;i++){
+        cout<<display.front()<<" ";
+         display.push(display.front());
+        display.pop();  
+    }
+    cout<<endl;
+    cout<<display.size();
+
+
+
 }
