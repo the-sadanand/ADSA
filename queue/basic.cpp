5cf4043d387f5b00ebb3b4be9207d751768eee0e
the-sadanand <thesadanand.toponed0@gmail.com>
2025-09-20 18:05:13 +0530

    queue basic

---
+++ queue/basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    // push,front , size , empty , pop, back
+    queue<int> q;
+    q.push(34);
+    q.push(4);
+    q.push(14);
+    q.push(324);
+    q.push(36);
+    cout<<q.front();
+}
