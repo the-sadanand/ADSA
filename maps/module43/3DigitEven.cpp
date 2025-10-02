32243a2a31248bae58d8efe3984235a9503a19a7
the-sadanand <thesadanand.toponed0@gmail.com>
2025-10-02 21:22:59 +0530

    Leetcode 2094 problem solve using map

---
+++ maps/module43/3DigitEven.cpp
@@ -0,0 +1,33 @@
+// leetcode 2094
+
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        int n = digits.size();
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(int ele : digits){
+            m[ele]++;
+        }
+        for(int i =100 ; i <= 999 ; i+=2){
+            int x=i;
+            int a = x%10; // ones place digit
+            x/=10;
+            int b = x%10;// tens place digit
+            x/=10;
+            int c = x;
+           if(m.find(a)!=m.end()){
+            m[a]--;
+            if(m[a]==0) m.erase(a);
+            if(m.find(b)!=m.end()){
+                m[b]--;
+                if(m[b]==0) m.erase(b);
+                if(m.find(c)!=m.end())ans.push_back(i);
+                m[b]++;
+            }
+            m[a]++;
+           }
+        }
+        return ans;
+    }
+};
