15f55863159645b31bb359e31f09f5a98d496787
the-sadanand <thesadanand.toponed0@gmail.com>
2026-07-03 22:29:27 +0530

    sales man problem

--- module_53/traveling_salseman_problem.cpp
+++ module_53/traveling_salseman_problem.cpp
@@ -1,37 +1,45 @@
 #include<iostream>
 #include<vector>
 #include<cstring>
+#include<climits>
+#include<algorithm>
+
 using namespace std;
-vector<vector<int>>grid={
+
+vector<vector<int>> grid = {
     {0,20,42,25},
     {20,0,30,34},
     {42,30,0,10},
     {25,34,10,0}
 };
 
-int dp[10][(1<<10)];
-int tsp(int curr , int mask , int n){
-    // curr -> curr node 
-    // mask -> set to denote the visited // 0011 
-    // n -> n for number of node 
+int dp[10][1<<10];
 
-    // base case 
-    if(mask-1 == (1<<n)){
-        return grid[arr][0];
-    }
-    if(dp[curr][mask]!=-1) return dp[curr][mask];
-    int ans = INT_MAX
-    for(int neigh = 0 ; neigh < n ; neigh++){
-        // case of not visited
-        if(mask & (1<<neigh)==0){
-            ans = min(ans,grid[curr][neigh]+tsp(neigh,mask|(1<<neigh),n));
+int tsp(int curr, int mask, int n)
+{
+    if(mask == (1<<n)-1)
+        return grid[curr][0];
+
+    if(dp[curr][mask] != -1)
+        return dp[curr][mask];
+
+    int ans = INT_MAX;
+
+    for(int i=0;i<n;i++)
+    {
+        if((mask & (1<<i)) == 0)
+        {
+            ans = min(ans,
+                      grid[curr][i] +
+                      tsp(i, mask | (1<<i), n));
         }
     }
+
     return dp[curr][mask] = ans;
 }
 
-
-int main(){
+int main()
+{
     memset(dp,-1,sizeof(dp));
-    cout<<tsp(0,1,4);
+    cout << tsp(0,1,4);
 }
