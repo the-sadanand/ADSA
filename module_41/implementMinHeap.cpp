8cb54fe154182c54c10086754727597b080f8f9a
the-sadanand <thesadanand.toponed0@gmail.com>
2026-07-15 23:45:45 +0530

    complement implementation of min heap using array

--- module_41/implementMinHeap.cpp
+++ module_41/implementMinHeap.cpp
@@ -17,6 +17,7 @@ class MinHeap{
     void push(int x){
         arr[idx]=x;
         int i = idx;
+        idx++;
         while(i!=1){
             int parent= i/2;
             if(arr[parent]>arr[i]){
@@ -26,8 +27,56 @@ class MinHeap{
             int i = parent;
         }
     }
+
+    void pop(){
+        idx--;
+        arr[1]=arr[idx];
+        int i = 1; 
+        while(true){
+            int left = 2*i , right = 2*i+1;
+            if(left>idx-1) break;
+            if(right>idx-1){
+                if(arr[i]>arr[left]){
+                    swap(arr[i],arr[left]);
+                    i=left;
+                }
+                break;
+            }
+            if(arr[left]>arr[right]){
+                if(arr[i]>arr[left]){
+                    swap(arr[i],arr[left]);
+                    i=left;
+                }
+                else break;
+            }
+            else {
+                if(arr[i]>arr[right]) {
+                    swap(arr[i],arr[right]);
+                    i=right;
+                }
+                else break;
+            }
+        }
+    }
+
+    void display(){
+        for(int i = 1 ; i<= idx-1 ; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
     int size(){
         return idx-1;
     }
 };
+
+int main(){
+    MinHeap pq;
+    pq.push(10);
+    pq.push(20);
+    // pq.push(1);
+    cout<<pq.size()<<endl;
+    pq.push(4);
+    cout<<pq.top()<<endl;
+}
 
