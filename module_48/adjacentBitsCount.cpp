20ef1e0ac9e644f0cdd34009832faa1e40e9d46f
the-sadanand <thesadanand.toponed0@gmail.com>
2026-08-04 21:40:05 +0530

    dynamic programming : adjacentBitsCount

--- module_48/adjacentBitsCount.cpp
+++ module_48/adjacentBitsCount.cpp
@@ -1,1 +1,35 @@
-#include<
+#include<iostream>
+#include<string.h>
+using namespace std;
+int dp[105][105][2];
+int f(int n, int k, int last)
+{
+    if(k < 0) return 0;
+
+    if(n == 0) return 0;
+
+    if(n == 1)
+        return (k == 0);
+
+    if(dp[n][k][last] != -1)
+        return dp[n][k][last];
+
+    if(last == 1)
+        return dp[n][k][last] =
+            f(n-1, k-1, 1) + f(n-1, k, 0);
+
+    return dp[n][k][last] =
+        f(n-1, k, 1) + f(n-1, k, 0);
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int num , n, k ;
+        cin>>num>>n>>k;
+        memset(dp, -1, sizeof dp);
+        int ans = f(n,k,0)+f(n,k,1);
+        cout<<num<<" "<<ans<<endl;
+    }
+    return 0;
+}
